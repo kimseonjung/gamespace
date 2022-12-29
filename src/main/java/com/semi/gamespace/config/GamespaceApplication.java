ebe8cb084a484d62a4f57bede25e32937c6c97c7
kimseonjung <sjkim3515@gmail.com>
2022-12-29 23:56:30 +0900

    controller 생성

--- src/main/java/com/semi/gamespace/config/GamespaceApplication.java
+++ src/main/java/com/semi/gamespace/config/GamespaceApplication.java
@@ -11,3 +11,4 @@ public class GamespaceApplication {
     }
 //        테스트3
 }
+
---
+++ src/main/java/com/semi/gamespace/game/controller/GameInfoController.java
@@ -0,0 +1,7 @@
+package com.semi.gamespace.game.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class GameInfoController {
+}
