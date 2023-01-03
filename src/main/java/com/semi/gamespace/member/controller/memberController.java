0a15563136eb3e06806c3c3f4c2d53b955c45f51
JinNyeongLee <wlssud@mensakorea.org>
2023-01-03 17:03:11 +0900

    로그인 페이지 초기 구현 완료

---
+++ src/main/java/com/semi/gamespace/member/controller/memberController.java
@@ -0,0 +1,13 @@
+package com.semi.gamespace.member.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/member")
+public class memberController {
+
+    @GetMapping("/login")
+    public void memberLoginForm() {}
+}
