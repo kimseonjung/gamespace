fb4b814e42e3fc0faff8c55e124a35a21f4927e3
YOUNGLEE <spacekingdom99@gmail.com>
2023-01-04 11:17:40 +0900

    2023/01/04 헤더링크연결 커밋

--- src/main/java/com/semi/gamespace/common/GameSpaceComon.java
+++ src/main/java/com/semi/gamespace/common/GameSpaceComon.java
@@ -1,4 +1,12 @@
 package com.semi.gamespace.common;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/main")
 public class GameSpaceComon {
+    @GetMapping("/index")
+    public void indexMain(){}
 }
