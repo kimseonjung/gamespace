20ea7ab522ba37fd49c9791a9352c5ae70fbcb31
YOUNGLEE <spacekingdom99@gmail.com>
2023-01-05 20:57:36 +0900

    2023/01/05 마지막 커밋

--- src/main/resources/static/js/fileInput.js
+++ src/main/resources/static/js/fileInput.js
@@ -1,4 +1,11 @@
-$("#file").on('change',function(){
-    var fileName = $("#file").val();
-    $(".upload-name").val(fileName);
-})
+$('#chooseFile').bind('change', function () {
+    var filename = $("#chooseFile").val();
+    if (/^\s*$/.test(filename)) {
+        $(".file-upload").removeClass('active');
+        $("#noFile").text("파일첨부");
+    }
+    else {
+        $(".file-upload").addClass('active');
+        $("#noFile").text(filename.replace("C:\\fakepath\\", ""));
+    }
+});
