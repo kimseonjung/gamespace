dc0cae80503c7b13d0319bf5f0bd8d69f9575d90
김선중 <sjkim3515@gmail.com>
2023-01-11 09:44:33 +0900

    gameInfo 20230111아침커밋푸쉬머지

--- src/main/java/com/semi/gamespace/config/ContextConfiguration.java
+++ src/main/java/com/semi/gamespace/config/ContextConfiguration.java
@@ -5,9 +5,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.servlet.view.BeanNameViewResolver;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+
+import java.util.concurrent.Executor;
 
 @Configuration
 @ComponentScan(basePackages = "com.semi.gamespace")
+@EnableAsync
 public class ContextConfiguration {
 
     @Bean
@@ -17,5 +24,31 @@ public class ContextConfiguration {
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
     }
+
+    @Bean(name = "jsonView")
+    MappingJackson2JsonView jsonView(){
+        return new MappingJackson2JsonView();
+    }
+
+
+    @Bean
+    public Executor getAsyncExecutor(){
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(8);
+        executor.setMaxPoolSize(8);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("seung-pool-");
+        executor.initialize();
+        return executor;
+
+    }
+
+
+
+
+
+
+
+
 
 }
--- src/main/java/com/semi/gamespace/game/controller/GameInfoController.java
+++ src/main/java/com/semi/gamespace/game/controller/GameInfoController.java
@@ -1,14 +1,16 @@
 package com.semi.gamespace.game.controller;
 
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.semi.gamespace.game.model.dto.*;
 import com.semi.gamespace.game.model.service.GameInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -34,23 +36,11 @@ public class GameInfoController {
 
 
 
-    @GetMapping("/game")
+    @GetMapping("game")
     public ModelAndView selectGameMainList(ModelAndView mv, HttpServletRequest request, HttpServletResponse response){
-
-        response.setContentType("application/json; charset=UTF-8");
 
         List<GameInfoDTO> gameInfoList = gameInfoService.selectAllGameInfo();
         List<CategoryDTO> categoryList = gameInfoService.selectAllCategory();
-
-        Enumeration params = request.getParameterNames();
-        while(params.hasMoreElements()) {
-            String name = (String) params.nextElement();
-            System.out.print(name + " : " + request.getParameter(name) + "     ");
-        }
-        System.out.println();
-        System.out.println("1111111111111111111111111111111111111111111111111111111111111111111111111111111");
-        List<CategoryDTO> categoryList2 = gameInfoService.selectOneCategory(request.getParameter("categoryNameBtn"));
-
         List<TagDTO> tagList = gameInfoService.selectAllTag();
         List<DevicesDTO> devicesList = gameInfoService.selectAllDevices();
 
@@ -59,16 +49,53 @@ public class GameInfoController {
         tagList.stream().forEach(tag -> System.out.println("tag = " + tag));
         devicesList.stream().forEach(devices ->System.out.println("devices = " + devices));
 
+
+
         mv.addObject("gameInfoList", gameInfoList);
         mv.addObject("categoryList", categoryList);
         mv.addObject("tagList", tagList);
         mv.addObject("devicesList", devicesList);
-        mv.addObject("categoryList2", categoryList2);
         mv.setViewName("game/gameMain");
 
         return mv;
 
     }
+
+    @PostMapping("game")
+    public ModelAndView selectGameMainJsonList( HttpServletRequest request, HttpServletResponse response) {
+        ModelAndView mv = new ModelAndView("jsonView");
+        response.setContentType("application/json; charset=UTF-8");
+
+        Gson gson = new GsonBuilder()
+                .setDateFormat("yyyy-MM-dd hh:mm:ss:SSS")
+                .setPrettyPrinting()
+                .setFieldNamingPolicy(FieldNamingPolicy.IDENTITY)
+                .serializeNulls()
+                .disableHtmlEscaping()
+                .create();
+
+
+
+        List<CategoryDTO> categoryList = gameInfoService.selectOneCategory(request.getParameter("categoryCode"));
+
+        mv.addObject("categoryList", gson.toJson(categoryList));
+        //mv.setViewName("jsonView");
+
+        return mv;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     @GetMapping("/gameInfoDetail")
