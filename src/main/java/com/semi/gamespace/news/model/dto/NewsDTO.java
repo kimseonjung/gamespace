05835f6b46a020c243dd6f2946d0d0f15eb2e762
spacekingdom <spacekingdom99@gmail.com>
2023-01-17 09:46:57 +0900

    2023/01/17 아침회의 및 파일커밋

--- src/main/java/com/semi/gamespace/news/model/dto/NewsDTO.java
+++ src/main/java/com/semi/gamespace/news/model/dto/NewsDTO.java
@@ -1,7 +1,16 @@
 package com.semi.gamespace.news.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.sql.Date;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class NewsDTO {
 
     private String newsCode;
@@ -12,85 +21,6 @@ public class NewsDTO {
     private String gameName;
     private String memberName;
 
-    public NewsDTO() {
-    }
+    private String isAdmin;
 
-    public NewsDTO(String newsCode, String newsTitle, String newsView, Date newsDate, String newsContent, String gameName, String memberName) {
-        this.newsCode = newsCode;
-        this.newsTitle = newsTitle;
-        this.newsView = newsView;
-        this.newsDate = newsDate;
-        this.newsContent = newsContent;
-        this.gameName = gameName;
-        this.memberName = memberName;
-    }
-
-    public String getNewsCode() {
-        return newsCode;
-    }
-
-    public void setNewsCode(String newsCode) {
-        this.newsCode = newsCode;
-    }
-
-    public String getNewsTitle() {
-        return newsTitle;
-    }
-
-    public void setNewsTitle(String newsTitle) {
-        this.newsTitle = newsTitle;
-    }
-
-    public String getNewsView() {
-        return newsView;
-    }
-
-    public void setNewsView(String newsView) {
-        this.newsView = newsView;
-    }
-
-    public Date getNewsDate() {
-        return newsDate;
-    }
-
-    public void setNewsDate(Date newsDate) {
-        this.newsDate = newsDate;
-    }
-
-    public String getNewsContent() {
-        return newsContent;
-    }
-
-    public void setNewsContent(String newsContent) {
-        this.newsContent = newsContent;
-    }
-
-    public String getGameName() {
-        return gameName;
-    }
-
-    public void setGameName(String gameName) {
-        this.gameName = gameName;
-    }
-
-    public String getMemberName() {
-        return memberName;
-    }
-
-    public void setMemberName(String memberName) {
-        this.memberName = memberName;
-    }
-
-    @Override
-    public String toString() {
-        return "NewsDTO{" +
-                "newsCode='" + newsCode + '\'' +
-                ", newsTitle='" + newsTitle + '\'' +
-                ", newsView='" + newsView + '\'' +
-                ", newsDate=" + newsDate +
-                ", newsContent='" + newsContent + '\'' +
-                ", gameName='" + gameName + '\'' +
-                ", memberName='" + memberName + '\'' +
-                '}';
-    }
 }
